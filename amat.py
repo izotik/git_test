564e14eba5ec5e8cfab09e76ad2e690ea22ad3b0
izotik <63355129+izotik@users.noreply.github.com>
2020-10-24 23:06:09 +0300

    Add files via upload

--- amat.py
+++ amat.py
@@ -1,6 +1,6 @@
-
 def cyctem(message):
     from nameamat import mat
     for m in mat:
+
         if m in message:
             return False
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import logging
+import logging, random
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
@@ -10,6 +10,8 @@ logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token='1337088673:AAFVcbSKpYLsvVqPy1eAjeBml0Ho5_jrlHA')
 dp = Dispatcher(bot, storage=MemoryStorage())
+
+
 ##dp.middleware.setup(LoggingMiddleware())
 
 
@@ -22,6 +24,7 @@ class TestStates(Helper):
     TEST_STATE_3 = ListItem()
     TEST_STATE_4 = ListItem()
     TEST_STATE_5 = ListItem()
+    TEST_STATE_6 = ListItem()
 
 
 @dp.message_handler(commands=['start'])
@@ -31,15 +34,18 @@ async def fg(message: types.message):
     await state.set_state(TestStates.all()[0])
 
 
-s = 0
-
 @dp.message_handler(state=TestStates.TEST_STATE_0)
 async def ma(message: types.message):
     state = dp.current_state(user=message.from_user.id)
     m = message.text
     if not amat.cyctem(m):
+        print(1)
         await message.answer('я всё твоей маме расскажу')
         await state.set_state(TestStates.all()[1])
+    else:
+        await state.set_state(TestStates.all()[6])
+        print(2)
+
 
 @dp.message_handler(state=TestStates.TEST_STATE_1)
 async def ma(message: types.message):
@@ -49,6 +55,7 @@ async def ma(message: types.message):
         await message.answer('сделал скриншот')
         await state.set_state(TestStates.all()[2])
 
+
 @dp.message_handler(state=TestStates.TEST_STATE_2)
 async def ma(message: types.message):
     state = dp.current_state(user=message.from_user.id)
@@ -56,6 +63,7 @@ async def ma(message: types.message):
     if not amat.cyctem(m):
         await message.answer('Отправил')
         await state.set_state(TestStates.all()[3])
+
 
 @dp.message_handler(state=TestStates.TEST_STATE_3)
 async def ma(message: types.message):
@@ -65,6 +73,7 @@ async def ma(message: types.message):
         await message.answer('Очередное неотесоное быдло')
         await state.set_state(TestStates.all()[4])
 
+
 @dp.message_handler(state=TestStates.TEST_STATE_4)
 async def ma(message: types.message):
     state = dp.current_state(user=message.from_user.id)
@@ -73,12 +82,31 @@ async def ma(message: types.message):
         await  message.answer_sticker('CAACAgIAAxkBAAI5pl-UB2XnSUmiOzLWIx0vlSg2_YaOAAKRAAPkoM4H4iu9K4yuq9YbBA')
         await state.set_state(TestStates.all()[5])
 
+
 @dp.message_handler(state=TestStates.TEST_STATE_5)
 async def ma(message: types.message):
     state = dp.current_state(user=message.from_user.id)
     m = message.text
     if not amat.cyctem(m):
+        print(1)
         await message.answer_sticker('CAACAgIAAxkBAAI5ml-T9poSqyFXdtycOalUBjny4E3jAAIOAAPANk8TI1cURIdu1mUbBA')
+
+
+@dp.message_handler(state=TestStates.TEST_STATE_6)
+async def ma(message: types.message):
+    await message.answer('Извини у меня не бесконечные возможность')
+    await message.answer('програмистам было лень создать мне искуственый интелект')
+    await message.answer_sticker('CAACAgIAAxkBAAI6CF-UgiXyyPkJ0djfigjKq_H8svV0AAIKBwACRvusBFNTipZg0G-tGwQ')
+
+
+@dp.message_handler(commands=['билеты'])
+async def ma(message: types.message):
+    while message.text != 'стоп':
+        md = ['сложно']
+        md.append(message.text)
+        print(md)
+    secure_random = random.SystemRandom()
+    await message.answer(secure_random)
 
 
 if __name__ == '__main__':
